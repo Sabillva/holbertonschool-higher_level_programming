b4ef93093e5fcc20b095324460326e6075174fa3
Sabillva <itifstudentsabina@gmail.com>
2024-09-04 15:49:58 +0000

    3-print_alphabt.py

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-for i in range(ord('a'), ord('z')+1):
-    if i == 'q' or i == 'e':
-        continue
-    print("{:c}".format(i), end="")
+for i in range(97, 123):
+    if chr(i) is not 'q' and chr(i) is not 'e':
+        print("{}".format(chr(i)), end="")
