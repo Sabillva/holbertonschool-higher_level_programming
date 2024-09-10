7e155d3f132dd234b22ab86790f4ff8eb40479f8
Sabillva <itifstudentsabina@gmail.com>
2024-09-10 16:30:38 +0000

    8-multiple_returns.py

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    for i in sentence:
-        if sentence == "":
-            return (0, None)
-        else:
-            return (len(sentence), sentence[0])
+    if sentence == "":
+        return (0, None)
+    return (len(sentence), sentence[0])
