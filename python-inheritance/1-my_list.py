de16b37881d026f20465d1c6af75761e3cecc41b
Sabillva <itifstudentsabina@gmail.com>
2024-09-28 11:29:23 +0000

    1-my_list.py, tests/1-my_list.txt

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,27 +1,27 @@
 #!/usr/bin/python3
 """
 Module for MyList which inherits from list.
-This class provides a method to print the list in sorted order.
 """
+
+
 
 class MyList(list):
     """
-    MyList class that inherits from the built-in list class.
-    It allows printing the list in sorted order.
+    elements of the list int type
+    return my list and sorted list
     """
 
     def print_sorted(self):
         """
-        Prints the list in ascending order.
-        The list is expected to contain integers only.
+        Prints the list in sorted order.
         """
         print(sorted(self))
 
     def append(self, item):
         """
         Appends an item to the list.
-        The item must be an integer, raises TypeError otherwise.
         """
         if not isinstance(item, int):
             raise TypeError("Item must be an integer")
         super().append(item)
+
