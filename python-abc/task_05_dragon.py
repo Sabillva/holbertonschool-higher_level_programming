73b5dae2c208c3d8cb6d0d4039d8b8d5fc668ae2
Sabillva <itifstudentsabina@gmail.com>
2024-09-28 12:09:22 +0000

    task6

--- python-abc/task_05_dragon.py
+++ python-abc/task_05_dragon.py
@@ -1,1 +1,18 @@
-task_05_dragon.py
+#!/usr/bin/python3
+"""Module with two mixin classes and Dragon class"""
+
+
+class SwimMixin:
+    """Swimmixing methods"""
+    def swim(self):
+        print("The creature swims!")
+
+class FlyMixin:
+    """FlyMixin methods"""
+    def fly(self):
+        print("The creature flies!")
+
+class Dragon(SwimMixin, FlyMixin):
+    """Dragon methods"""
+    def roar(self):
+        print("The dragon roars!")
