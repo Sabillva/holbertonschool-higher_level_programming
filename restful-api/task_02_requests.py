ae4715d67bc436f0c23876bac60133d822d55350
Sabillva <itifstudentsabina@gmail.com>
2024-10-15 22:26:05 +0400

    task2

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -17,3 +17,6 @@ def fetch_and_save_posts():
     url = 'https://jsonplaceholder.typicode.com/posts'
     response = requests.get(url)
     
+    if response.status_code == 200:
+        posts = response.json()
+        formatted_posts = [{'id': post['id'], 'title': post['title'], 'body': post['body']} for post in posts]
