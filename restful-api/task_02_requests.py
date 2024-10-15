1ffe5c6ccf1b0063384754d21d86a268cca0acc1
Sabillva <itifstudentsabina@gmail.com>
2024-10-15 22:22:21 +0400

    task2

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -7,3 +7,9 @@ def fetch_and_print_posts():
     url = 'https://jsonplaceholder.typicode.com/posts'
     response = requests.get(url)
     print(f'Status Code: {response.status_code}')
+    
+    if response.status_code == 200:
+        posts = response.json()
+        for post in postS:
+            print(post['title'])
+
