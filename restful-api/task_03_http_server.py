c4ba30e3b1261f7f34e48b9e529c8ec1a670f679
Sabillva <itifstudentsabina@gmail.com>
2024-10-16 23:34:48 +0400

    task3

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -4,7 +4,8 @@ import socketserver
 
 class NewHandler(http.server.BaseHTTPRequestHandler):
     def do_GET(self):
-         self.send_response(200)
-         self.send_header('Content-type', 'text/plain')
-         self.end_headers()
-         self.wfile.write(b'Hello, this is a simple API!')
+          if self.path == '/':
+              self.send_response(200)
+              self.send_header('Content-type', 'text/plain')
+              self.end_headers()
+              self.wfile.write(b'Hello, this is a simple API!')
