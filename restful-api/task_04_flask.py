25de529abe8c0a91b362b289566817ba6ef8f94f
Sabillva <itifstudentsabina@gmail.com>
2024-10-18 11:55:24 +0400

    task4

--- restful-api/task_04_flask.py
+++ restful-api/task_04_flask.py
@@ -3,3 +3,12 @@ from flask import Flask, request, jsonify
 app = Flask(__name__)
 
 users = {}
+
+@app.route("/")
+def home():
+    return "Welcome to the Flask API!"
+
+
+@app.route("/data")
+def get_data():
+    return jsonify(list(users.keys()))
