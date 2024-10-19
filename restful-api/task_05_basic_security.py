da76989f1b8711ba5377c9525b9a9739a3139ec4
Sabillva <itifstudentsabina@gmail.com>
2024-10-19 17:53:21 +0400

    task5

--- restful-api/task_05_basic_security.py
+++ restful-api/task_05_basic_security.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
 """Module for secure API"""
 from flask import Flask, jsonify, request
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_httpauth import HTTPBasicAuth
+from flask_jwt_extended import (JWTManager, create_access_token, jwt_required, get_jwt_identity)
